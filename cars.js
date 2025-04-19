35fffa7ba145f3af9080e4795c6380d48821b8a6
Ayoub <ayoubmoutaouakil577@gmail.com>
2025-04-19 16:28:26 +0100

    cars.js

--- cars.js
+++ cars.js
@@ -1,5 +1,17 @@
 let menu = document.querySelector(".menu-icon");
+let navbar = document.querySelector(".navbar");
+
 menu.onclick = () => {
     menu.classList.toggle("move");
-
+    navbar.classList.toggle("open-menu");
 };
+
+const animate = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: '1500',
+    delay: '400',
+})
+
+animate.reveal('.nav');
+animate.reveal(".home-img img", { origin: "right"});
