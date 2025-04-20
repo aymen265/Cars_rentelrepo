3bca0f9e6accd9f79dd23de9b96bbe9bd81f783d
Aymen Lahlia <aymanehilya@gmail.com>
2025-04-20 12:50:54 +0100

    Update form.php

--- form.php
+++ form.php
@@ -1,28 +1,27 @@
 <?php
+  session_start();
+
+?>
+<?php
 try {
-    $conn = new PDO("mysql:host=localhost;dbname=cars;port=3306;charset=utf8", 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $conn = new PDO("mysql:host=localhost;dbname=cars;port=3307;charset=utf8", 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 } catch (Exception $e) {
     echo 'Erreur de connexion: ' . $e->getMessage();
 }
-if (isset($_POST['full-name']) && isset($_POST['email']) && isset($_POST['tel']) && isset($_POST['pickup-date']) && isset($_POST['return-date']) && isset($_POST['location'])&& isset($_POST['notes'])&& isset($_POST['payement'])) {
-    if (!empty($_POST['full-name']) && !empty($_POST['email']) && !empty($_POST['tel']) && !empty($_POST['pickup-date']) && !empty($_POST['return-date']) && !empty($_POST['location']) && !empty($_POST['notes'])&& !empty($_POST['payement'])) {
 
-        $nomC = htmlspecialchars($_POST['full-name']);
-        $email = htmlspecialchars($_POST['email']);
-        $pdate = htmlspecialchars($_POST['pickup-date']);
-        $rdate = htmlspecialchars($_POST['return-date']);
-        $location = htmlspecialchars($_POST['location']);
-        $notes = htmlspecialchars($_POST['notes']);
-        $tel = htmlspecialchars($_POST['tel']);
-        $payement = htmlspecialchars($_POST['payement']);
+
+        $nomC = htmlspecialchars($_SESSION['full-name']);
+        $email = htmlspecialchars($_SESSION['email']);
+        $pdate = htmlspecialchars($_SESSION['pickup-date']);
+        $rdate = htmlspecialchars($_SESSION['return-date']);
+        $location = htmlspecialchars($_SESSION['location']);
+        $notes = htmlspecialchars($_SESSION['notes']);
+        $tel = htmlspecialchars($_SESSION['tel']);
+        $payement = htmlspecialchars($_SESSION['payement']);
         
 
         $insertionClient = $conn->prepare('INSERT INTO carstable(`full_name`,`email_adress`,`pick-up_date`,`return_date`,`pick-up_location`,`notes`,`tel`,`payement_method`) VALUES(?,?,?,?,?,?,?,?)');
         $insertionClient->execute(array($nomC,$email,$pdate,$rdate,$location,$notes,$tel,$payement));
 
-        echo 'Le Client a été bien ajouté !!';
-    } else {
-        echo 'Attention, Tous Les Champs Sont Obligatoires !!';
-    }
-}
+        header("location:afficher.php")
 ?>
